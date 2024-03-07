cb31807cca177c6d143a3b8ae41d760a2d024be1
tomasz <t.limisiewicz@gmail.com>
2024-03-07 12:07:46 +0100

    Added sheduler to fine-tuning.

--- src/ft_generative_task.py
+++ src/ft_generative_task.py
@@ -4,8 +4,10 @@ import accelerate
 from accelerate import Accelerator
 import argparse
 import torch
+import transformers
 from functools import partial
 import os
+from copy import copy
 
 from flores_modeling import get_model_tokenizer, get_flores_data
 from utils import normalize_text
@@ -14,7 +16,7 @@ import csv
 from itertools import islice, cycle
 from collections import defaultdict
 
-N_EVAL_BATCHES = 20
+N_EVAL_BATCHES = 50
 
 TRANSLATION_LANGUAGES = ['ta', 'te', 'el', 'hy', 'ru', 'kk', 'am', 'vi', 'ja', 'fr', 'sm', 'st', 'ko', 'de', 'mt', 'pl', 'sn', 'en']
 
@@ -81,21 +83,23 @@ def get_dataset(tokenizer, task, directory, shrads=10):
 	train_datasets = [train_eval_dataset.shard(num_shards=shrads, index=i) for i in range(shrads-1)]
 	eval_dataset = train_eval_dataset.shard(num_shards=shrads, index=shrads-1)
 
-	train_loaders = [DataLoader(dataset.map(partial(preprocess_function, tokenizer=tokenizer), batched=True, desc="Running tokenizer"), batch_size=8, shuffle=True) for dataset in train_datasets]
-	eval_loader = DataLoader(eval_dataset.map(partial(preprocess_function, tokenizer=tokenizer), batched=True, desc="Running tokenizer"), batch_size=5, shuffle=True)
-	test_loader = DataLoader(test_dataset.map(partial(preprocess_function, tokenizer=tokenizer), batched=True, desc="Running tokenizer"), batch_size=5, shuffle=True)
+	train_loaders = [DataLoader(dataset.map(partial(preprocess_function, tokenizer=tokenizer), batched=True, desc="Running tokenizer"), batch_size=4, shuffle=True) for dataset in train_datasets]
+	eval_loader = DataLoader(eval_dataset.map(partial(preprocess_function, tokenizer=tokenizer), batched=True, desc="Running tokenizer"), batch_size=2, shuffle=True)
+	test_loader = DataLoader(test_dataset.map(partial(preprocess_function, tokenizer=tokenizer), batched=True, desc="Running tokenizer"), batch_size=2, shuffle=True)
 
 	return train_loaders, eval_loader, test_loader
 
 
-def train_evaluate(model, train_loaders, eval_loader, lr=1e-4, n_epochs=30, patience=3):
-	accelerator = Accelerator(gradient_accumulation_steps=8, mixed_precision="bf16")
+def train_evaluate(model, train_loaders, eval_loader, lr=1e-4, n_epochs=30, orig_patience=2):
+	patience=orig_patience
+	accelerator = Accelerator(gradient_accumulation_steps=16, mixed_precision="bf16")
 	device = accelerator.device
 	model.to(device)
 	optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 	optimizer = accelerate.optimizer.AcceleratedOptimizer(optimizer)
+	scheduler = transformers.get_inverse_sqrt_schedule(optimizer, num_warmup_steps=100)
 	train_loaders = list(islice(cycle(accelerator.prepare(train_loaders)), n_epochs))
-	model, optimizer, eval_loader = accelerator.prepare(model, optimizer, eval_loader)
+	model, optimizer, scheduler, eval_loader = accelerator.prepare(model, optimizer, scheduler, eval_loader)
 	for epoch, train_loader in enumerate(train_loaders):
 		epoch_loss = 0.
 		model.train()
@@ -109,6 +113,7 @@ def train_evaluate(model, train_loaders, eval_loader, lr=1e-4, n_epochs=30, pati
 				loss = outputs.loss
 				accelerator.backward(loss)
 				optimizer.step()
+				scheduler.step()
 			epoch_loss += loss.item() / float(len(train_loader))
 		# eval on a portion of the eval_loader
 		del input_ids, attention_mask, labels, outputs, loss
@@ -132,18 +137,24 @@ def train_evaluate(model, train_loaders, eval_loader, lr=1e-4, n_epochs=30, pati
 			patience -= 1
 		else:
 			prev_loss = avg_eval_loss
+			patience = orig_patience
+			model_copy = copy(model)
 
 		if patience == 0:
 			print('Early stopping due to increase in loss')
 			break
 
-	return model
+	return model_copy
 
 
-def infer(model, tokenizer, evaluation_loader, model_type, model_size, directory,  split='test'):
+def infer(model, tokenizer, evaluation_loader, model_type, model_size, directory,  split='test', experiment_name=None):
 
-	with open(f"{directory}/{split}_{model_type}_{model_size}.csv", 'w', encoding='utf-8') as eval:
-		writer = csv.writer(eval, quoting=csv.QUOTE_ALL)
+	if experiment_name is not None:
+		out_file = f"{directory}/{split}_{model_type}_{model_size}_{experiment_name}.csv"
+	else:
+		out_file = f"{directory}/{split}_{model_type}_{model_size}.csv"
+	with open(out_file, 'w', encoding='utf-8') as out:
+		writer = csv.writer(out, quoting=csv.QUOTE_ALL)
 		writer.writerow(["Id", "Language" ,"Predicted", "Expected"])
 		for i, batch in tqdm(enumerate(evaluation_loader)):
 			if "Id" not in batch:
@@ -164,8 +175,9 @@ if __name__ == "__main__":
 	argparser.add_argument("--model_type", required=True, type=str)
 	argparser.add_argument("--model_size", required=True, type=str)
 
-	argparser.add_argument("--patience", default=3, type=int)
+	argparser.add_argument("--patience", default=2, type=int)
 	argparser.add_argument("--lr", default=1e-4, type=float)
+	argparser.add_argument("--experiment_name", default=None, type=str)
 	argparser.add_argument("--model_steps", required=False, type=int, default=250000)
 
 	args = argparser.parse_args()
@@ -173,7 +185,7 @@ if __name__ == "__main__":
 	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 	fine_tune = True
-	if os.path.isdir(f"{args.checkpoint_dir}/{args.model_type}_{args.model_size}_{args.model_steps}_{args.task}") is True:
+	if os.path.isdir(f"{args.checkpoint_dir}/{args.model_type}_{args.model_size}_{args.model_steps}_{args.task}") is True and args.experiment_name is None:
 		print("Fine-tuned model exists, loading for evaluation")
 		fine_tune = False
 	model, tokenizer = get_model_tokenizer(args.model_type, args.model_size, args.model_steps, args.checkpoint_dir, device=device, task=args.task)
@@ -181,8 +193,11 @@ if __name__ == "__main__":
 	train_loaders, eval_loader, test_loader = get_dataset(tokenizer, args.task, args.directory)
 
 	if fine_tune:
-		model = train_evaluate(model, train_loaders, eval_loader, lr=args.lr, patience=args.patience)
-		model.save_pretrained(f"{args.checkpoint_dir}/{args.model_type}_{args.model_size}_{args.task}", use_safetensors=True)
+		model = train_evaluate(model, train_loaders, eval_loader, lr=args.lr, orig_patience=args.patience)
+		if args.experiment_name is None:
+			model.save_pretrained(f"{args.checkpoint_dir}/{args.model_type}_{args.model_size}_{args.task}", use_safetensors=True)
 
-	infer(model, tokenizer, eval_loader, args.model_type, args.model_size, args.directory, split='dev')
-	infer(model, tokenizer, test_loader, args.model_type, args.model_size, args.directory, split='submission' if args.task == "spelling_correction" else 'devtest')
+	infer(model, tokenizer, eval_loader, args.model_type, args.model_size, args.directory,
+	      split='dev', experiment_name=args.experiment_name)
+	infer(model, tokenizer, test_loader, args.model_type, args.model_size, args.directory,
+	      split='submission' if args.task == "spelling_correction" else 'devtest', experiment_name=args.experiment_name)
